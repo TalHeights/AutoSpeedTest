9d31fdfb8e71a18b5af194ce36ddd2e785c3d555
Tal Laptop <tal@heights-t.com>
2024-02-19 11:29:24 +0200

    Testing with the best server when running

--- AutoSpeedTest.py
+++ AutoSpeedTest.py
@@ -1,7 +1,8 @@
 import math
 import speedtest
 
-def Bytes_to_MegaBytes(size_bytes):
+# Setup:
+def Bytes_to_MegaBytes(size_bytes): # this function converting from bytes to megabytes.
     i = int(math.floor(math.log(size_bytes, 1024)))
     power = math.pow(1024, i)
     size = round(size_bytes / power, 2)
@@ -9,11 +10,19 @@ def Bytes_to_MegaBytes(size_bytes):
 
 wifi = speedtest.Speedtest()
 
+ServerValue = wifi.get_best_server() # Store best server in "ServerValue".
+
+print('Best server by latency is: ' + ServerValue.get('sponsor') + ", Location: " + ServerValue.get('name') + ", ID: " + ServerValue.get('id') + ".")
+
+# Test loop:
+wifi.get_servers(servers=[ServerValue.get('id')]) # For specipic server replace "ServerValue.get('id')" to other ID.
+print("Testing with: " + ServerValue.get('sponsor'))
+
 print("Getting download speed...")
-download_speed = wifi.download()
+download_speed = wifi.download() # Getting download speed.
 
 print("Getting upload speed...")
-upload_speed = wifi.upload()
+upload_speed = wifi.upload() # Getting upload speed.
 
 print("Download: ", Bytes_to_MegaBytes(download_speed))
 print("Upload: ", Bytes_to_MegaBytes(upload_speed))
